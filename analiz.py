7d15e4de1c8ad2f96c633e2dfffa6c758d3f4478
kozal2026 <erdalkozal5@hotmail.com>
2026-03-30 14:27:11 +0300

    Update analiz.py

--- analiz.py
+++ analiz.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Erkoz Analiz", layout="centered")
 st.title("🚴‍♂️ Erkoz Yazılım - Sürüş Analizi")
 
 # Senin linkini buraya sabitledim kanka
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1Z4WxyRA3Q3bUtvu29ZebnRIal10554fIQvut9uoVOZY/edit?usp=sharing"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1Z4WxyRA3Q3bUtvu29ZebnRIal10554fIQvut9uoVOZY/edit"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
